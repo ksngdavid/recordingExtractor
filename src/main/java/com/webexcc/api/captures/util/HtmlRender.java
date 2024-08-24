85008c06662b768228183455dcee36d6c6334dc5
ksngdavid <ksngdavid@gmail.com>
2024-08-24 16:56:43 +0800

    Adding Report Only

--- src/main/java/com/webexcc/api/captures/util/HtmlRender.java
+++ src/main/java/com/webexcc/api/captures/util/HtmlRender.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.webexcc.api.captures.util.HtmlRender;
 import com.webexcc.api.captures.service.ApiService;
 import com.webexcc.api.model.Capture;
 import com.webexcc.api.model.CaptureAttributes;
