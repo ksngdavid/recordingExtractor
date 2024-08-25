a87a7ff22b20cfb06d3e1047b307ff0f07600d4a
ksngdavid <ksngdavid@gmail.com>
2024-08-25 10:34:49 +0800

    Adding Report Only

--- src/main/java/com/webexcc/api/captures/util/HtmlRender.java
+++ src/main/java/com/webexcc/api/captures/util/HtmlRender.java
@@ -253,7 +253,7 @@ public class HtmlRender {
 		//String reportV = "no";
 		sb.append("\nString (\"reportV\") = \"no\";\n");
 		//sb.append("\n");
-		sb.append("\n<return (\"reportV\");\n");
+		//sb.append("\n<return (\"reportV\");\n");
 		//return "{\"Exception\":\"" + e.getMessage() + "\"}";
 		sb.append("\n</script>");
 		sb.append("\n");
@@ -319,7 +319,7 @@ public class HtmlRender {
 
 		sb.append("</tr>");
 
-	return "{(\"reportV\")}";
+	return reportV;
 	}
 
 	public void header(StringBuffer sb) {
