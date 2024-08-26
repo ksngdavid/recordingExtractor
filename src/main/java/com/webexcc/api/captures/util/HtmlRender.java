048667979ea334f171b0f41cea153848aa90082e
ksngdavid <ksngdavid@gmail.com>
2024-08-26 17:54:07 +0800

    Adding Report Only

--- src/main/java/com/webexcc/api/captures/util/HtmlRender.java
+++ src/main/java/com/webexcc/api/captures/util/HtmlRender.java
@@ -246,7 +246,7 @@ public class HtmlRender {
 		sb.append("<span> &nbsp; &nbsp; &nbsp;<span/>");
 		sb.append("\n<script>");
 		sb.append("\nfunction submit2() {");
-		sb.append("\nString \"reportV\" = 'no';\n");	
+		sb.append("\n\"reportV\" = "no";\n");	
 		sb.append("document.getElementById(\"tableWait\").style.display = \"block\";\n");
 		sb.append("\ndocument.getElementById('main').submit();");
 		//sb.append("\nreturn reportV;");
@@ -309,7 +309,7 @@ public class HtmlRender {
 		}
 
 		sb.append("</br>");
-		sb.append("<button type='button' onClick='javaScript:return submit2();'>Submit</button>");
+		sb.append("<button type='button' onClick='javaScript:submit2();'>Submit</button>");
 		sb.append("<button type='button' onClick='javaScript:submit3();'>Report Only</button>");
 
 		sb.append("</form>");
